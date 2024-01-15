de353aa5a2af2b032f7e22e3d9832c295e485cea
mantrobuski <spellbattle4@gmail.com>
2024-01-15 15:06:49 -0800

    black side initialized

--- team-00/src/main/java/ubc/cosc322/COSC322Test.java
+++ team-00/src/main/java/ubc/cosc322/COSC322Test.java
@@ -1,6 +1,7 @@
 
 package ubc.cosc322;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -20,8 +21,8 @@ public class COSC322Test extends GamePlayer{
     private GameClient gameClient = null; 
     private BaseGameGUI gamegui = null;
 	
-    private String userName = "Memeson";
-    private String passwd = "yeet";
+    private String userName = null;
+    private String passwd = null;
  
 	
     /**
@@ -55,17 +56,24 @@ public class COSC322Test extends GamePlayer{
     	
     	//To make a GUI-based player, create an instance of BaseGameGUI
     	//and implement the method getGameGUI() accordingly
-    	//this.gamegui = new BaseGameGUI(this);
+    	this.gamegui = new BaseGameGUI(this);
     }
  
 
 
     @Override
     public void onLogin() {
-    	System.out.println("Congratualations!!! "
-    			+ "I am called because the server indicated that the login is successfully");
-    	System.out.println("The next step is to find a room and join it: "
-    			+ "the gameClient instance created in my constructor knows how!"); 
+    	
+    	List<Room> rooms = gameClient.getRoomList();
+    	
+    	gameClient.joinRoom("Kalamalka Lake");
+    	
+    	
+    	userName = gameClient.getUserName();
+    	if(gamegui != null) 
+    	{
+    		gamegui.setRoomInformation(rooms);
+    	}
     }
 
     @Override
@@ -75,8 +83,29 @@ public class COSC322Test extends GamePlayer{
 	
     	//For a detailed description of the message types and format, 
     	//see the method GamePlayer.handleGameMessage() in the game-client-api document. 
+    	
+    	System.out.println("type: " + messageType);
+    	for (Map.Entry<String,Object> entry : msgDetails.entrySet())
+    	{
+    		System.out.println("Key: " + entry.getKey());
+    		if(entry.getKey() == "game-state")
+        	{
+        		System.out.println("setting game state");
+        		this.getGameGUI().setGameState((ArrayList<Integer>) entry.getValue());
+        	}
+    	}
+    	
+    	
+    	
     	    	
     	return true;   	
+    }
+    
+    @Override
+    public boolean handleMessage(String msg)
+    {
+    	System.out.println("Generic msg: " + msg);
+    	return true;
     }
     
     
@@ -94,7 +123,7 @@ public class COSC322Test extends GamePlayer{
 	@Override
 	public BaseGameGUI getGameGUI() {
 		// TODO Auto-generated method stub
-		return  null;
+		return this.gamegui;
 	}
 
 	@Override

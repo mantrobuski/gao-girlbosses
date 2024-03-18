9539250299b19e9ad648b0d43abf5a59c0aab5a5
Mason <spellbattle4@gmail.com>
2024-03-18 04:04:03 -0700

    Territory heuristic

--- team-00/src/main/java/ubc/cosc322/GameState.java
+++ team-00/src/main/java/ubc/cosc322/GameState.java
@@ -59,7 +59,6 @@ public class GameState
 	//0 indexed arrays, 1 indexed board notation because ???
 	public static int yxToIndex(int y, int x)
 	{
-		System.out.println("Returning: ");
 		return (y - 1) + 10* (x - 1);
 	}
 	
@@ -230,6 +229,161 @@ public class GameState
 		else 	
 			return output;
 	}
+	
+	
+	//***********************************************************************
+	//swap out the function called here to use a different heuristic function
+	//***********************************************************************
+	public int evalutate() 
+	{
+		return territoryHeuristic();
+	}
+	
+	
+	private int territoryHeuristic()
+	{
+		//we are going to count the total number of tiles each queen sees, black counts for negative
+		int territory = 0;
+		
+		
+		//create two array lists containing the location of each queen (index notation)
+		ArrayList<Integer> whiteQueens = new ArrayList<Integer>();
+		ArrayList<Integer> blackQueens = new ArrayList<Integer>();
+		for(int i = 0; i < this.board.length; i++)
+		{
+			//add the index of the queens we care about
+			if(board[i] == 1) whiteQueens.add(i);
+			if(board[i] == -1) blackQueens.add(i);
+		}
+		
+		//for each white queen
+		for(int queen : whiteQueens)
+		{
+			//check 4 directions
+			
+			//up
+			int cursor = queen;
+			cursor++;
+			do
+			{
+				if(cursor > 99 || cursor < 0) break;
+				else if(board[cursor] == 0)
+				{
+					territory += 1;
+				}
+				else break;
+				cursor++;
+			}while(cursor % 10 != 0);
+			
+			//down
+			cursor = queen;
+			cursor--;
+			do
+			{
+				if(cursor > 99 || cursor < 0) break;
+				else if(board[cursor] == 0)
+				{
+					territory += 1;
+				}
+				else break;
+				cursor--;
+			}while(cursor % 10 != 9);
+			
+			//right
+			cursor = queen;
+			cursor += 10;
+			do
+			{
+				if(cursor > 99 || cursor < 0) break;
+				else if(board[cursor] == 0)
+				{
+					territory += 1;
+				}
+				else break;
+				cursor += 10;
+			}while(cursor >  9);
+			
+			//left
+			cursor = queen;
+			cursor -= 10;
+			do
+			{
+				if(cursor > 99 || cursor < 0) break;
+				else if(board[cursor] == 0)
+				{
+					territory += 1;
+				}
+				else break;
+				cursor -= 10;
+			}while(cursor < 90);
+		}
+		
+		//for each black queen
+		for(int queen : blackQueens)
+		{
+			//check 4 directions
+			
+			//up
+			int cursor = queen;
+			cursor++;
+			do
+			{
+				if(cursor > 99 || cursor < 0) break;
+				else if(board[cursor] == 0)
+				{
+					territory -= 1;
+				}
+				else break;
+				cursor++;
+			}while(cursor % 10 != 0);
+			
+			//down
+			cursor = queen;
+			cursor--;
+			do
+			{
+				if(cursor > 99 || cursor < 0) break;
+				else if(board[cursor] == 0)
+				{
+					territory -= 1;
+				}
+				else break;
+				cursor--;
+			}while(cursor % 10 != 9);
+			
+			//right
+			cursor = queen;
+			cursor += 10;
+			do
+			{
+				if(cursor > 99 || cursor < 0) break;
+				else if(board[cursor] == 0)
+				{
+					territory -= 1;
+				}
+				else break;
+				cursor += 10;
+			}while(cursor >  9);
+			
+			//left
+			cursor = queen;
+			cursor -= 10;
+			do
+			{
+				if(cursor > 99 || cursor < 0) break;
+				else if(board[cursor] == 0)
+				{
+					territory -= 1;
+				}
+				else break;
+				cursor -= 10;
+			}while(cursor < 90);
+		}
+		
+		
+		return territory;
+	}
+	
 	
 	@Override
 	public boolean equals(Object other)
